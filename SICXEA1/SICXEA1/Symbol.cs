9fc6ef40ac4668a70a6a84ea9193134e2bacff32
NathanAamodt50 <79725211+NathanAamodt50@users.noreply.github.com>
2021-09-16 21:01:55 -0500

    Add files via upload

---
+++ SICXEA1/SICXEA1/Symbol.cs
@@ -0,0 +1,49 @@
+﻿/* Nathan Aamodt
+ * Systems Programming
+ * Assign 1
+ * Hamer
+ * This program will take symbol inputs from symbol.dat create a table using thoes inputs.
+ * Then it will search from Test.txt to search for symbols in the table.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SICXEA1
+{
+    public class Symbol
+    {
+        public string Sym { get; set; }
+        public int RF { get; set; }
+        public int Value { get; set; }
+        public int MF { get; set; }
+        /*
+         * Symbol
+         * This function sets up the table with all the entities it needs for each entry
+         * Inputs are S,R,M,F
+         * doesnt output anything but is used to create the whole list
+         */
+        public Symbol(string S = "", int R = 0, int M = 0, int F =0)
+        {
+            Sym = S;
+            RF = R;
+            Value = M;
+            MF = F;
+        }
+        /*
+ * Symbol
+ * This function sets up the table with all the entities it needs for each entry
+ * Inputs are other symbol method having it named to create a copy of it
+ * doesnt output anything but is used to create the whole list
+ */
+        public Symbol(Symbol Soul)
+        {
+            Sym = Soul.Sym;
+            RF = Soul.RF;
+            Value = Soul.Value;
+            MF = Soul.MF;
+        }
+    }
+}
---
+++ SICXEA1/SICXEA1/View.cs
@@ -0,0 +1,44 @@
+﻿/* Nathan Aamodt
+ * Systems Programming
+ * Assign 1
+ * Hamer
+ * This program will take symbol inputs from symbol.dat create a table using thoes inputs.
+ * Then it will search from Test.txt to search for symbols in the table.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SICXEA1
+{
+    public static class View
+    {
+        /*
+         * ViewSym
+         * This function takes the table and prints it out in groups of 5
+         * This requires the List to be input so it has something to print
+         */
+        public static void ViewSym(List L)
+        {
+            Console.WriteLine("Symbol\t\tRFlag\tMflag\tValue");
+            for (int i = 0; i < L.Table.Length; i++)
+            {
+                if (i == 5 || i == 10 || i == 15 || i == 20 || i == 25 || i ==30)
+                {
+                    Console.WriteLine("Press a key to continue!");
+                    Console.ReadKey(true);
+                    Console.WriteLine(L.Table[i].Sym + "  \t " + L.Table[i].RF +"\t" + L.Table[i].MF+ "  \t   " + L.Table[i].Value);
+                }
+                else
+                {
+                    Console.WriteLine(L.Table[i].Sym + "  \t " + L.Table[i].RF + "\t" + L.Table[i].MF + "  \t  " + L.Table[i].Value);
+                }
+            }
+
+
+            return;
+        }
+    }
+}
